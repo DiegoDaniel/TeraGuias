6f2decaaff7c2e11dd1a257c791021292cdeaf2f
Diego Galarza <galarza.diego@hotmail.com>
2019-06-28 23:42:50 -0300

    Arreglo para NavBar

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ $(document).ready(function() {
 	
 	$("#navBarHtml").load("templates/navBar.html", function(){
 		$("#labelMenuNav").addClass("d-none");
+
+		$(".dropdown").on("click",function(event){
+			event.preventDefault();
+			event.stopPropagation()
+		});
+
 	});
+	
 	
 });
