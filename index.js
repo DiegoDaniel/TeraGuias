b0f86ad6882a1383f15fe705b66db24a563832ed
Diego Galarza <galarza.diego@hotmail.com>
2019-06-29 01:08:51 -0300

    Update nocturno 01:08

--- index.js
+++ index.js
@@ -3,12 +3,9 @@ $(document).ready(function() {
 	$("#navBarHtml").load("templates/navBar.html", function(){
 		$("#labelMenuNav").addClass("d-none");
 
-		$(".dropdown").on("click",function(event){
+		$(".nav-link").on("click",function(event){
 			event.preventDefault();
 			event.stopPropagation()
 		});
-
-	});
-	
-	
+	});	
 });
